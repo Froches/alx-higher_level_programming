4268284bc428f8e49f38ccf0237ad9f957477f74
Froches <michaelfavour418@gmail.com>
2023-08-15 00:17:32 -0700

    Task 9. find max int

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max_val = my_list[0]
+    for i in my_list:
+        if i > max_val:
+            max_val = i
+
+    return max_val
