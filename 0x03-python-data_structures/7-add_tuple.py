69cc3dbf3aa5cc3b668164b898e06074aaa45a9f
Froches <michaelfavour418@gmail.com>
2023-08-14 23:55:05 -0700

    Task 7. add tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = tuple_a + (0, 0)[:2 - len(tuple_a)]
+    tuple_b = tuple_b + (0, 0)[:2 - len(tuple_b)]
+
+    result_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+
+    return result_tuple
+
