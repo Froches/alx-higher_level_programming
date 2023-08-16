83f171de332d3d9ca07f835a0e1f3422e6fd4168
Froches <michaelfavour418@gmail.com>
2023-08-16 12:58:14 -0700

    Task 1. calculate

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,16 @@
-__import__("calculator_!")
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    res_add = add(a, b)
+    res_sub = sub(a, b)
+    res_mul = mul(a, b)
+    res_div = div(a, b)
+
+    print("{} + {} = {}".format(a, b, res_add))
+    print("{} - {} = {}".format(a, b, res_sub))
+    print("{} * {} = {}".format(a, b, res_mul))
+    print("{} / {} = {}".format(a, b, res_div))
