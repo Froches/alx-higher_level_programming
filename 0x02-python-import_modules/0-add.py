523865c58ddaed5681b6c9eaff0cd785470c9e62
Froches <michaelfavour418@gmail.com>
2023-08-16 12:32:31 -0700

    Task 0. add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
-def add(a, b):
-    return a + b
 
-if __name__ == "__main__":
-    a = 1
-    b = 2
+a = 1
+b = 2
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
+from add_0 import add
+
+sums = add(a, b)
+print("{} + {} = {}".format(a, b, sums))
