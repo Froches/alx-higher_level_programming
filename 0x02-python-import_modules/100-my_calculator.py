bb87d03f2153ef15ad47ddd477fded1555a4cbaf
Froches <michaelfavour418@gmail.com>
2023-08-18 23:23:35 -0700

    Task 6. My calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
 
-if __name__ == "__main__":
+if len(sys.argv) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
 
-    from calculator_1 import add, sub, mul, div
-    import sys
+a = int(sys.argv[1])
+operator = sys.argv[2]
+b = int(sys.argv[3])
 
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+if operator == "+":
+    result = add(a, b)
+elif operator == "-":
+    result = sub(a, b)
+elif operator == "*":
+    result = mul(a, b)
+elif operator == "/":
+    result = div(a, b)
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+print(f"{a} {operator} {b} = {result}")
