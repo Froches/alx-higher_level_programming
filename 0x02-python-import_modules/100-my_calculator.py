0cfe3b4c6e1543a09a110058f702501057be7b60
Froches <michaelfavour418@gmail.com>
2023-08-18 23:15:30 -0700

    Task 6. my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+
     from calculator_1 import add, sub, mul, div
     import sys
 
-    if len(sys.argv) != 4:
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
-
-    if operator not in ['+', '-', '*', '/']:
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    if operator == '+':
-        result = a + b
-    elif operator == '*':
-        result = a * b
-    elif operator == '-':
-        result = a - b
-    else:
-         if b == 0:
-             print("Error: Division by zero is not allowed.")
-             sys.exit(1)
-             result = a / b
-
-             print("{} {} {} = {}".format(a, operator, b, result))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
