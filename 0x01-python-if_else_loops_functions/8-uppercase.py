6adf3f49ef0831641bd68ba85b961c7bbbf5fafc
Froches <michaelfavour418@gmail.com>
2023-08-21 01:34:17 -0700

    Task 8. uppercase. Fixed!

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
-     for char in str:
+    result = ""
+
+    for char in str:
         if ord(char) >= 97 and ord(char) <= 122:
             upper_char = chr(ord(char) - ord('a') + ord('A'))
-            print("{}".format(upper_char), end='')
+            result += "{}".format(upper_char)
         else:
-            print("{}".format(char), end='')
+            result += "{}".format(char)
+
+    print(result)
