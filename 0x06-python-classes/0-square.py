c8e8998418817ad02525396f37b6f3b4b7649c18
Froches <michaelfavour418@gmail.com>
2023-08-30 05:54:27 -0700

    documentation fixed

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
 
+# class Square:
+#  """
+#  A simple class representing a square
+#  Attributes:
+#     No attributes are defined for this class.
+#  """
+#   pass
 class Square:
     """
-    A simple class representing a square
+    A simple class representing a square.
 
     Attributes:
         No attributes are defined for this class.
-"""
-    pass
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of the Square class.
+
+        This constructor doesn't require any parameters.
+        """
+        pass
