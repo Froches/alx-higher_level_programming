31e863a625d51ea8cfeb8ce562a3ee191cd11cbd
Froches <michaelfavour418@gmail.com>
2023-09-02 12:30:38 -0700

    Task

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        formatted_value = ("{:d}".format(int(value)))
+        print(formatted_value)
+        return True
+    except Exception:
+        return False
