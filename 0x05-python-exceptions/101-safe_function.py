2b550ef6862078fb1fd7f29cf8fae749a7f21e9a
Froches <michaelfavour418@gmail.com>
2023-09-03 14:22:04 -0700

    Safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as e:
+        sys.stderr.write("Exception: " + str(e) + "\n")
+        return None
