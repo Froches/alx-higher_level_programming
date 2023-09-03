690bdff152d496de8ca3d7dcf857c95cc7ffbc8d
Froches <michaelfavour418@gmail.com>
2023-09-03 08:20:13 -0700

    Task 4. list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -17,7 +17,7 @@ def list_division(my_list_1, my_list_2, list_length):
                 new_list.append(elem1 / elem2)
 
     except IndexError:
-            print("out of range")
+            print("division by 0")
             new_list.append(0)
     finally:
         pass
