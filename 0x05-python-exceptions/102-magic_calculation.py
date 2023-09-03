cfb909cf5ed93310e7d57c6027e3dcae44beb5ab
Froches <michaelfavour418@gmail.com>
2023-09-03 15:27:58 -0700

    Bytecode.

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -9,7 +9,7 @@ def magic_calculation(a, b):
             else:
                 result += (a ** b) / i
         except Exception:
-            result += b + a
+            result = b + a
             break
 
         return result
