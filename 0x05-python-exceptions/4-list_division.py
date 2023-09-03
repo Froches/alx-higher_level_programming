1ab75d4ebdaef7b87ae2f94be4bbba81ba62c0f7
Froches <michaelfavour418@gmail.com>
2023-09-03 08:16:08 -0700

    Task 4. list division

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    try:
+        for i in range(list_length):
+            elem1 = my_list_1[i] if i < len(my_list_1) else 0
+            elem2 = my_list_2[i] if i < len(my_list_2) else 0
+
+            if not isinstance(elem1, (int, float)) or not isinstance(elem2, (int, float)):
+                print("wrong type")
+                new_list.append(0)
+            elif elem2 == 0:
+                print("division by 0")
+                new_list.append(0)
+            else:
+                new_list.append(elem1 / elem2)
+
+    except IndexError:
+            print("out of range")
+            new_list.append(0)
+    finally:
+        pass
+
+    return new_list
