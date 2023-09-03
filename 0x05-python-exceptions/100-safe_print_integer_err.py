0af2d76bee267ae909c5a551e188b304e40463f0
Froches <michaelfavour418@gmail.com>
2023-09-03 09:13:42 -0700

    Print int with error message

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    try:
+        if value == int(value):
+            print("{:d}".format(value))
+            return True
+    except ValueError:
+        return False
