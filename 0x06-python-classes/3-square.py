db327ee410c43d4f0b9d2d3034035578e302aeb4
Froches <michaelfavour418@gmail.com>
2023-09-09 23:56:31 -0700

    Task 3. area of square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a square class"""
+
+
+class Square:
+    """The square class"""
+
+    def __init__(self, size=0):
+        """
+        Initializing my new square
+
+        Args:
+            size: The size of the square
+
+        Attributes:
+            area: Calculates the area of square
+        """
+        self.__size = size
+
+        nat = isinstance(size, int)
+        if nat is not True:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Returns the area of the square"""
+        return (self.__size * self.__size)
