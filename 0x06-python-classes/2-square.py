d71da9cd9ba498c0ea9ebe51abc9d5793da3d54b
Froches <michaelfavour418@gmail.com>
2023-09-09 23:38:07 -0700

    Task 2. size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a square class"""
+
+
+class Square:
+    """The square class"""
+
+    def __init__(self, size=0):
+        """
+        Initializing my new square
+
+        Args:
+            size: The size of the square
+        """
+        self.__size = size
+
+        nat = isinstance(size, (float, int))
+        if nat != True:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
