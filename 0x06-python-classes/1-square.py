5140487a1e99fd02fc59f8158a6ed93a5f0aa993
Froches <michaelfavour418@gmail.com>
2023-09-09 23:16:47 -0700

    Task 1. __size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """The square"""
+
+    def __init__(self, size):
+        """Initializing a new square
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
