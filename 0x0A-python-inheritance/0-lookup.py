e84c7b0d715af5194c5a7c97d440d6b6fad928d3
Froches <michaelfavour418@gmail.com>
2023-09-13 09:06:45 -0700

    Task 0. Lookup.

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """A function that returns the list of attributes and methods of an object"""
+
+
 def lookup(obj):
+    """The lookup function.
+    Args:
+        obj: object passed whose attributes we're to find
+    """
+
     attr_meth = dir({obj})
-    avail = [item for item in attr_meth] #  if not item.startswith('_')]
+    avail = [item for item in attr_meth]
     return avail
