16fd940fc2aa5e18f12cd380568689f35a80a507
Froches <michaelfavour418@gmail.com>
2023-09-16 14:25:20 -0700

    Task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Adds two ints"""
+
+def add_integer(a, b=98):
+    """
+    Returns a+b
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a + b)
