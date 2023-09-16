f1ef07884b9ef7c1892f3148e1de01a3ddb45d84
Froches <michaelfavour418@gmail.com>
2023-09-16 10:54:04 -0700

    is kind of class

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Function that checks if an object is an instance of a class or its parent class"""
+"""Function that checks if an object is an
+instance of a class or its parent class"""
+
 
 def is_kind_of_class(obj, a_class):
-    """Checks if an object is an instance of a class or one that inherited from that class"""
+    """Checks if an object is an instance of a
+    class or one that inherited from that class"""
     if isinstance(obj, a_class):
         return True
     return False
