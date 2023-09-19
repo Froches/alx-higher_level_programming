000fa9609617d061e1fab930fc12554936b2c1b6
Froches <michaelfavour418@gmail.com>
2023-09-19 05:30:19 -0700

    Rectangle. raise type error and value error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -21,8 +21,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("width must be greater than zero")
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -31,8 +33,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
         if value <= 0:
-            raise ValueError("height must be greater than zero")
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -41,8 +45,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("X coordinate cannot be negative")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -51,6 +57,8 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("Y coordinate cannot be negative")
+            raise ValueError("y must be >= 0")
         self.__y = value
