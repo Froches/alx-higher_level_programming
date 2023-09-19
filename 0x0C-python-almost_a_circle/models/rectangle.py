11a629571e1c3ceecef338832b94567c6c0ff335
Froches <michaelfavour418@gmail.com>
2023-09-18 17:07:21 -0700

    Task 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Creating a crectangle class that inherits from base"""
+from models.base import Base
+
+class Rectangle(Base):
+    """
+    A rectangle class that inherits from the Base class
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, value):
+        if value <= 0:
+            raise ValueError("width must be greater than zero")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if value <= 0:
+            raise ValueError("height must be greater than zero")
+        self.__width = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if value < 0:
+            raise ValueError("X coordinate cannot be negative")
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if value < 0:
+            raise ValueError("Y coordinate cannot be negative")
+        self.__y = value
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Rectangle testing class"""
+
+
+from models.rectangle import Rectangle
+
+
