fd9442cab61beac58a5414930c92f06a07bbfc45
Favour Michael <michaelfavour418@gmail.com>
2023-09-21 09:19:34 -0700

    json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """Class Base"""
 
 
+import json
+
 class Base:
     """
     Represents a class Base which will act as a base class for
@@ -16,3 +18,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Implementing the json method"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
