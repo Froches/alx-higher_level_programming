e112d25b54171ce0a3e0e87aa999d49a6f703a34
Froches <michaelfavour418@gmail.com>
2023-10-09 18:11:59 -0700

    unittest

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """Unit tests for base.py"""
-
-
 import unittest
 from models.base import Base
 
@@ -22,6 +20,16 @@ class TestBase(unittest.TestCase):
         obj2 = Base()
         self.assertEqual(obj1.id, 1)
         self.assertEqual(obj2.id, 2)
+
+    def test_auto_id_assignment(self):
+        obj1 = Base()
+        obj2 = Base()
+        obj3 = Base()
+
+        assert obj1.id != obj2.id
+        assert obj2.id != obj3.id
+        assert obj3.id != obj1.id
+
 
 if __name__ == '__main__':
     unittest.main()
