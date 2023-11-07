30dd5f10b4210578db165fb076ad543e8e030a73
Froches <michaelfavour418@gmail.com>
2023-11-07 05:57:40 -0800

    added tests

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,34 @@
 #!/usr/bin/python3
 """Adds two ints"""
 
+
 def add_integer(a, b=98):
     """
-    Returns a+b
+    This function adds two integers
+    It works with integers
+
+    >>> add_integer(2, 3)
+    5
+
+    >>> add_integer(-1, 1)
+    0
+
+    >>> add_integer(0, 0)
+    0
+
+    But does not work with strings or characters
+
+    >>> add_integer('s', 10)
+    Traceback (most recent call last):
+    File "<stdin>", line 1, in <module>
+    File "<stdin>", line 3, in add_integer
+    TypeError: a must be an integer
+
+    >>> add_integer(5, 'g')
+    Traceback (most recent call last):
+    File "<stdin>", line 1, in <module>
+    File "<stdin>", line 5, in add_integer
+    TypeError: b must be an integer
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
