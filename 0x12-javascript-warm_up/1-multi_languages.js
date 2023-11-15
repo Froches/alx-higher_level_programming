0b10328207b8335f68e431da56e3a6017f4bbc2f
Favour Michael <michaelfavour418@gmail.com>
2023-11-14 19:39:08 -0800

    arguments

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,4 +3,3 @@
 console.log('C is fun');
 console.log('Python is cool');
 console.log('Javascript is amazing');
-
--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-
-argv.forEach((val, index) => {
-  console.log(`${index}: ${val}`);
-});
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
