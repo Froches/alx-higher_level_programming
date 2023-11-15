b72b78aea9c88587d4f5c0dc208499b623c234d1
Favour Michael <michaelfavour418@gmail.com>
2023-11-14 20:58:53 -0800

    rect 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+  rotate() {
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+}
+module.exports = Rectangle;
