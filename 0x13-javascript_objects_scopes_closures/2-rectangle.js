c422db57bb6896b0c54205610bb1e2b517b610c6
Favour Michael <michaelfavour418@gmail.com>
2023-11-14 20:47:39 -0800

    semistandard fixed

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || Number.isInteger(h))
-    {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || Number.isInteger(h)) {
       return;
     }
     this.width = w;
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || Number.isInteger(h)) {
       return;
     }
@@ -8,10 +8,10 @@ class Rectangle {
     this.height = h;
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
 }
-module.exports =Rectangle;
+module.exports = Rectangle;
