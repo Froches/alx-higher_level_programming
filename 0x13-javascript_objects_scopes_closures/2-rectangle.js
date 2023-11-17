a7c72286ac19f5fe0c367f8d1dc6079771e37e00
Favour Michael <michaelfavour418@gmail.com>
2023-11-17 07:12:08 -0800

    Task 2. Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || Number.isInteger(h)) {
-      return;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 }
-module.exports = Rectangle;
+// module.exports.Rectangle = Rectangle;
