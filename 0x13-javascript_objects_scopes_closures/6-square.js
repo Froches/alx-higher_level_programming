86fea9cceec801a91cf556b2bfccca48bcc1a67f
Favour Michael <michaelfavour418@gmail.com>
2023-11-17 12:38:54 -0800

    next square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Square1 = require('./5-square');
+
+class Square extends Square1 {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
