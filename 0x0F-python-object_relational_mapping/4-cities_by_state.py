4d00b1413012da5c3f27e7077ff0f81e4e2b934b
Froches <michaelfavour418@gmail.com>
2023-11-19 16:44:23 -0800

    Cities by states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script that lists all states with a name starting with N from a database
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+            cities INNER JOIN states ON states.id=cities.state_id""")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
