a2328a203e9a472f5ad73ccdc57f42639d76f1c5
Froches <michaelfavour418@gmail.com>
2023-11-24 07:36:11 -0800

    all with an a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa"""
+"""
+Script that lists all State objects that contain the
+letter a from the database hbtn_0e_6_usa
+"""
 
 import sys
 from sqlalchemy import create_engine
@@ -14,7 +17,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).filter(State.name.like('%a%')).order_by(State.id):
+    for state in session.query(State)\
+            .filter(State.name.like('%a%')).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
 
     session.close()
