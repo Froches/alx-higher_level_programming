bcc5225546f9fbe015a72b73bb0950fa12f23927
Froches <michaelfavour418@gmail.com>
2023-11-24 08:30:09 -0800

    city in state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 
+
 class City(Base):
     """Represents a city with SQLalchemy."""
 
