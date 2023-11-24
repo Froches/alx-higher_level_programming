463da689144f7565255d7de107361af62f82b292
Froches <michaelfavour418@gmail.com>
2023-11-24 08:36:00 -0800

    trying to get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,9 +10,10 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+    """The entry point"""
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+                           echo=False)
 
     Base.metadata.create_all(bind=engine)
     Session = sessionmaker(engine)
