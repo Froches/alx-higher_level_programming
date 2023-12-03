53a5d82eeda8f82ed31d5191575d094b8807b84f
Froches <michaelfavour418@gmail.com>
2023-12-02 20:37:56 -0800

    states table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Creates the database hbtn_0d_usa and the table states on my MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
