7e194e15d649da1a13c1b8c321b38adad9da1ab5
Favour Michael <michaelfavour418@gmail.com>
2024-01-16 10:52:11 +0100

    converter

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  if (typeof arguments[1] !== 'number' || isNaN(arguments[1])) {
-    throw new Error('Invalid input. Please provide a valid number.');
-  }
+  return function convertNumberToBase(number) {
+      if (isNaN(number) || isNaN(base) || base < 2 || base > 36) {
+          return "Invalid input";
+      }
 
-  const convertRecursive = (number, base) =>
-    number === 0 ? '' : convertRecursive(Math.floor(number / base), base) + (number % base);
-
-  return convertRecursive(arguments[1], base);
+      return number.toString(base);
+  };
 };
---
+++ 0x13-javascript_objects_scopes_closures/10-main.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+import { converter } from './10-converter';
+
+let myConverter = converter(10);
+
+console.log(myConverter(2));
+console.log(myConverter(12));
+console.log(myConverter(89));
+
+
+myConverter = converter(16);
+
+console.log(myConverter(2));
+console.log(myConverter(12));
+console.log(myConverter(89));
