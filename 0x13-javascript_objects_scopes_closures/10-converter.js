450d7222eb3c14a5e67df67ec40fc6c9e17084e5
Favour Michael <michaelfavour418@gmail.com>
2024-01-16 10:58:24 +0100

    fixed semistandard

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  return function convertNumberToBase(number) {
-      if (isNaN(number) || isNaN(base) || base < 2 || base > 36) {
-          return "Invalid input";
-      }
+  return function convertNumberToBase (number) {
+    if (isNaN(number) || isNaN(base) || base < 2 || base > 36) {
+      return 'Invalid input';
+    }
 
-      return number.toString(base);
+    return number.toString(base);
   };
 };
