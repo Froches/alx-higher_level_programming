8a860c347d4c57c44c425174d6b4a4a780650e38
Froches <michaelfavour418@gmail.com>
2024-02-26 06:36:51 -0800

    genre id by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,7 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 
+USE hbtn_0d_tvshows;
+
 SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id
 FROM hbtn_0d_tvshows.tv_shows
 JOIN hbtn_0d_tvshows.tv_show_genres ON tv_shows.id = tv_show_genres.show_id
